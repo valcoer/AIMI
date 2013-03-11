e7a0b4e0f83c7bb1594031a6ad1c9e245cfd695a
valcoer <arch_nemesis_hos@yahoo.com>
2013-03-10 23:17:13 -0500

    v1.0.0.0

---
+++ AIMi/AIMi/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AIMind
+{
+    public partial class Form1 : Form
+    {
+        public string salutation = "hello world";
+        public string regret = "I do not understand";
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "you have my attention";
+            this.button1.Visible = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+              Greetings greetings = new Greetings();
+            foreach (string greeting in greetings.Greet)
+            {
+                bool resultant;
+                resultant = this.textBox1.Text.Contains(greeting);
+
+                if (resultant == true)
+                {
+                    label1.Text = greeting;
+                    return;
+                }
+                else
+                {
+                    label1.Text = "is this a greeting?";
+                    button2.Visible = true;
+                    button3.Visible = true;
+
+                }
+            }
+
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string newGreeting = this.textBox1.Text;
+            
+            this.label1.Text = newGreeting;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+      
+       
+    }
+    public class Greetings
+    {
+        string[] _greet = { "hi", "hello", "greetings" };
+        public string[] Greet
+        {
+            get { return _greet; }
+        }
+        public string this[int index]
+        {
+            get { return _greet[index]; }
+        }
+
+
+       
+    }
+}
