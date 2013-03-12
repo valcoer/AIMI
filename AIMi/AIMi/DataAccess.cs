f5f22aaa0d38ddab50d3084f61e5513375991e04
valcoer <arch_nemesis_hos@yahoo.com>
2013-03-11 21:29:28 -0500

    v1.0.0.1

---
+++ AIMi/AIMi/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace AIMind
+{
+    class DataAccess
+    {
+        private string _path;
+        private string _localPath = @"";
+        XmlDocument doc;
+        public DataAccess(string xmlDocumentName)
+        {
+            _path = _localPath + xmlDocumentName;
+            doc = new XmlDocument();
+            doc.Load(_path);
+        }
+        public void AddWord(string word)
+        {
+            System.Xml.XmlElement newNode = new XmlElement() ;
+            newNode.InnerText = word;
+            
+            doc.SelectSingleNode("Words").AppendChild(newNode);
+            doc.Save(_path);
+        }
+    }
+   
+}
